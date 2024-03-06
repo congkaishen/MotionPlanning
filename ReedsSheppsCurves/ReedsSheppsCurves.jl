5b5c0dca80ce9142b72016b2e0fda824b8794c71
DESKTOP-2DSLOG9\Congkai Shen <cosh@umich.edu>
2024-03-05 21:39:19 -0500

    add test file for RS

--- ReedsSheppsCurves/ReedsSheppsCurves.jl
+++ ReedsSheppsCurves/ReedsSheppsCurves.jl
@@ -488,7 +488,7 @@ function allpath(states)
 end
 
 
-function plotactpaths(init_st, minR, cmds, costs)
+function plotActpaths(init_st, minR, cmds, costs)
     fea_idx = findall(costs.<Inf)
     h = plot()
 
@@ -528,29 +528,3 @@ end
 
 
 
-# x0 = 10*(2*rand(1)[1]-1)
-# y0 = 10*(2*rand(1)[1]-1)
-# ψ0 = pi*(2*rand(1)[1]-1)
-# init_st = [x0, y0, ψ0]
-
-
-# xg = 10*(2*rand(1)[1]-1)
-# yg = 10*(2*rand(1)[1]-1)
-# ψg = pi*(2*rand(1)[1]-1)
-
-# termi_st = [xg, yg, ψg]
-# minR = 10*(2*rand(1)[1]-1)
-# norm_states = changeBasis(init_st, termi_st, minR)
-# opt_cmd, opt_cost, cmds, costs = allpath(norm_states)
-# act_opt_cost = opt_cost*minR
-    
-
-
-# using Plots
-# h1 = plotpaths(cmds, costs)
-
-# h2 = plotactpaths(init_st, minR, cmds, costs)
-
-# println(init_st)
-# println(termi_st)
-# display(h2)
---
+++ ReedsSheppsCurves/test.jl
@@ -0,0 +1,29 @@
+include("./ReedsSheppsCurves.jl")
+
+
+x0 = 10*(2*rand(1)[1]-1)
+y0 = 10*(2*rand(1)[1]-1)
+ψ0 = pi*(2*rand(1)[1]-1)
+init_st = [x0, y0, ψ0]
+
+
+xg = 10*(2*rand(1)[1]-1)
+yg = 10*(2*rand(1)[1]-1)
+ψg = pi*(2*rand(1)[1]-1)
+
+termi_st = [xg, yg, ψg]
+minR = 10*(2*rand(1)[1]-1)
+norm_states = changeBasis(init_st, termi_st, minR)
+opt_cmd, opt_cost, cmds, costs = allpath(norm_states)
+act_opt_cost = opt_cost*minR
+    
+
+
+using Plots
+h1 = plotpaths(cmds, costs)
+
+h2 = plotActpaths(init_st, minR, cmds, costs)
+
+println(init_st)
+println(termi_st)
+display(h2)
