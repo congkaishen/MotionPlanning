2e81f0177c705d1ed175a8079aed881da21a7315
johnysyumich <johnysy@umich.edu>
2024-03-12 15:14:33 -0400

    Add plotting capability

--- PathPlanning/Dijkstra/main.jl
+++ PathPlanning/Dijkstra/main.jl
@@ -18,7 +18,7 @@ BoundPosition = [0; 100; 0; 100]
 obs_list = [[50,50,25],[15,15,8],[70,10,0],[10,70,10],[90,50,10],[50,90,10]]
 
 
-dka = defineDKA(BoundPosition, [51, 51], starting_pose[1:2], ending_pose[1:2], false)
+dka = defineDKA(BoundPosition, [51, 51], starting_pose[1:2], ending_pose[1:2], true)
 defineDKAobs!(dka, obs_list)
 @time planDKA!(dka)
 # plot(dka.r.actualpath[:,1], dka.r.actualpath[:,2])
--- PathPlanning/Dijkstra/src/types.jl
+++ PathPlanning/Dijkstra/src/types.jl
@@ -18,7 +18,7 @@ Base.isless(a::DKANode, b::DKANode) = isless(a.f, b.f)
     obstacle_list::Vector{Vector{Float64}} = Vector{Vector{Float64}}[]
     x_factor::Float64 = 0.0
     y_factor::Float64 = 0.0
-    draw_fig::Bool = false
+    draw_fig::Bool = true
 end
 
 @with_kw mutable struct DKAPlanner
